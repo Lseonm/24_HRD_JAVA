b8b712ca64ca17e30248a5598a7c4815e488349e
Lseonm <151723449+Lseonm@users.noreply.github.com>
2024-08-06 11:31:27 +0900

    Add files via upload
    첫 번째 예제

---
+++ Hello.java
@@ -0,0 +1,26 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		/**
+		 * 작성일 :
+		 * 작성자 :
+		 * 내용 :
+		 */
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		
+		for (int i=0; i<10; i++)
+		System.out.println("Hello Java");
+	}
+
+}
